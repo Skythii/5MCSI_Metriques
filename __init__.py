66e2dc97a7871db490848e615b6ff7cd4a1a974c
Skythii <tbo2marret@gmail.com>
2025-04-01 14:18:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,9 +11,14 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contact():
+    return render_template("contact.html")
+
 
 @app.route('/tawarano/')
 def meteo():
